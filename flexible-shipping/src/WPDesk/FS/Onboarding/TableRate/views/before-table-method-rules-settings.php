69f30bd08ea29237c265d1a3017667519e7b1bd1
Tuấn <nguy.minh.tuan@sun-asterisk.com>
2021-06-25 14:57:30 +0700

    add src

--- flexible-shipping/src/WPDesk/FS/Onboarding/TableRate/views/before-table-method-rules-settings.php
+++
@@ -1,9 +0,0 @@
-<div id="onboarding-container"></div>
-
-<div class="onboarding-open-container">
-	<strong>
-		<a href="#0" class="js--open-onboarding">
-			<?php _e( 'Check how the rules table works &rarr;', 'flexible-shipping' ); ?>
-		</a>
-	</strong>
-</div>
